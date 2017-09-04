77b6a5007e5a008e46a18e0662a6de70e36507f2
wcongke <wcongke@castles.com.cn>
2017-09-04 18:16:47 +0800

    feat: 允许跨域

--- src/app.js
+++ src/app.js
@@ -5,10 +5,20 @@ const Path = require('path')
 
 const app = new Koa()
 
-app.use(Views(Path.join(__dirname, `./views`), {
-  extension: 'ejs'
-}))
-app.use(Router.routes())
-app.use(Router.allowedMethods())
+app
+  .use(Views(Path.join(__dirname, `./views`), {
+    extension: 'ejs'
+  }))
+  .use((ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*')
+    ctx.set('Content-Type', 'application/json')
+    ctx.set('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin')
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS')
+    ctx.set('Access-Control-Allow-Credentials', true) // 允许带上 cookie
+    return next()
+  })
+  .use(Router.routes())
+  .use(Router.allowedMethods())
 
 module.exports = app
